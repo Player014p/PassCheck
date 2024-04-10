75cfaaf7a30fe08e2f9c6812d7ee4e58a3773c1a
player014p <program.player0@gmail.com>
2024-04-10 20:36:38 -0300

    Mensaje descriptivo del commit

---
+++ inicio.bat
@@ -0,0 +1,9 @@
+@echo off
+color a
+title Password Checker
+echo BIENVENIDO!!!
+echo script creado por player014p todos los derechos reservados
+echo --------------------------------------------------------
+:var
+python powned.py
+goto var
